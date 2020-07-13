5d19ad97795c5a731f6b3508d2c48f83d68ac8b3
lingjie.li <lingjie.li@amlogic.com>
2020-07-13 16:31:36 +0800

    meson-mali: release Bifrost fbdev backend [1/1]
    PD#SWPL-29489
    
    Problem:
    Need do Bifrost fbdev backend r16 release
    
    Solution:
    release fbdev backend DDK
    
    Verify:
    none
    
    Change-Id: Ia59f45a3bb8dd0c659a14f6d0fde2013dc955e91
    Signed-off-by: lingjie li <lingjie.li@amlogic.com>

--- include/EGL_platform/platform_dummy/eglplatform.h
+++ include/EGL_platform/platform_dummy/eglplatform.h
@@ -1,55 +1,65 @@
 /*
  * This confidential and proprietary software may be used only as
  * authorised by a licensing agreement from ARM Limited
- * (C) COPYRIGHT 2014-2016 ARM Limited
+ * (C) COPYRIGHT 2011-2014, 2017-2018 ARM Limited
  * ALL RIGHTS RESERVED
  * The entire notice above must be reproduced on all authorised
  * copies and copies may only be made to the extent permitted
  * by a licensing agreement from ARM Limited.
  */
 
-#ifndef __EGLPLATFORM_H__
-#define __EGLPLATFORM_H__
+/**
+ * @file eglplatform.h
+ * Platform related definitions
+ */
+
+#ifndef _EGLPLATFORM_H_
+#define _EGLPLATFORM_H_
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
+#endif /* __cplusplus */
+
+
+#include <KHR/khrplatform.h>
+#include <stdint.h>
+
+#include "mali_fbdev_types.h"
+
+#ifndef EGLAPI
+#define EGLAPI
 #endif
 
 #ifndef EGLAPIENTRY
 #define EGLAPIENTRY
 #endif
+
 #ifndef EGLAPIENTRYP
-#define EGLAPIENTRYP EGLAPIENTRY*
-#endif
-#ifndef EGLAPI
-#define EGLAPI extern
+#define EGLAPIENTRYP EGLAPIENTRY *
 #endif
 
-#include <KHR/khrplatform.h>
+typedef fbdev_window * EGLNativeWindowType;
 
-typedef struct wl_egl_window *NativeWindowType;
-typedef void *NativePixmapType;
-typedef void *NativeDisplayType;
+typedef int32_t EGLNativePixmapType;
+
+typedef dummy_display * EGLNativeDisplayType;
+
+typedef EGLNativeWindowType NativeWindowType;
+typedef EGLNativePixmapType NativePixmapType;
+typedef EGLNativeDisplayType NativeDisplayType;
+
+typedef int32_t EGLint;
 
 #ifdef __cplusplus
 }
+#endif /* __cplusplus */
+
+/* C++ / C typecast macros for special EGL handle values */
+#if defined(__cplusplus)
+#define EGL_CAST(type, value) (static_cast<type>(value))
+#else
+#define EGL_CAST(type, value) ((type) (value))
 #endif
 
-/* EGL 1.2 types, renamed for consistency in EGL 1.3 */
-typedef NativeDisplayType EGLNativeDisplayType;
-typedef NativePixmapType EGLNativePixmapType;
-typedef NativeWindowType EGLNativeWindowType;
-
-/* Define EGLint. This must be an integral type large enough to contain
- * all legal attribute names and values passed into and out of EGL,
- * whether their type is boolean, bitmask, enumerant (symbolic
- * constant), integer, handle, or other.
- * While in general a 32-bit integer will suffice, if handles are
- * represented as pointers, then EGLint should be defined as a 64-bit
- * integer type.
- */
-typedef int EGLint;
-typedef intptr_t EGLAttrib;
-
-#endif /* __EGLPLATFORM_H__ */
-
+#endif /* _EGLPLATFORM_H_ */
